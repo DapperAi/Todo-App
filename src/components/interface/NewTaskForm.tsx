8efb2ff15482f71a41f5961467321c4588e3c499
vgvishesh <vgvishesh2008@gmail.com>
2024-04-06 21:11:29 +0530

    Make the div around DatePicker in file 'newtaskform.tsx` have visible outline so that it is visible to the user

--- src/components/interface/NewTaskForm.tsx
+++ src/components/interface/NewTaskForm.tsx
@@ -35,10 +35,12 @@ const NewTaskForm: React.FC<NewTaskFormProps> = ({ onSubmit }) => {
           <SelectItem key="In Progress" value="In Progress">In Progress</SelectItem>
           <SelectItem key="Done" value="Done">Done</SelectItem>
         </Select>
-        <DatePicker
-          selected={dueDate}
-          onChange={(date) => setDueDate(date)}
-        />
+        <div className="ring-2 ring-gray-300">
+          <DatePicker
+            selected={dueDate}
+            onChange={(date) => setDueDate(date)}
+          />
+        </div>
         <Checkbox checked={reminder} onChange={(e) => setReminder(e.target.checked)}>Set Reminder</Checkbox>
       <Button type="submit">Create Task</Button>
     </form>
