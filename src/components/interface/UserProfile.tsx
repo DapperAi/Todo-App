27032eb7bf82fbceb92172acce09d06a7de227f9
vgvishesh <vgvishesh2008@gmail.com>
2024-04-09 18:00:21 +0530

    update the userProfile.tsx component, to display the user avatar in the horizontal center of the screen, and also display the username using a good style

--- src/components/interface/UserProfile.tsx
+++ src/components/interface/UserProfile.tsx
@@ -6,9 +6,9 @@ type UserProfileProps = {
 
 const UserProfile = (props: UserProfileProps) => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center justify-center">
       <Avatar isBordered color="primary" size="lg" src="https://i.pravatar.cc/150?u=a04258114e29026708c"/>
-      <div className="ml-2">{props.username}</div>
+      <div className="ml-2 text-lg font-semibold text-gray-800">{props.username}</div>
     </div>
   );
 };
